0f86c3ab37427d195ff31fe8db493fc6682584b4
Steven Wong <stevenwong2726@gmail.com>
2024-10-14 14:47:34 -0700

    added cors library

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -57,6 +58,7 @@ const deleteUser = (id) => {
 };
  
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
